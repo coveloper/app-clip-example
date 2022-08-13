dcde3fe07f6e550479320e271062dc87bc66b464
Jon Bauer <jon@covelop.org>
2022-08-13 16:46:46 -0700

    Hello world from parent host

--- AppClipExample/AppClipExample/ContentView.swift
+++ AppClipExample/AppClipExample/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, world from the Parent Host App!")
             .padding()
     }
 }
