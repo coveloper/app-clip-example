3011cb7632ece35d64d4b052a12fef4557d0786b
Jon Bauer <jon@covelop.org>
2022-08-13 16:45:03 -0700

    Initial Project Creation

---
+++ AppClipExample/AppClipExample/AppClipExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipExampleApp.swift
+//  AppClipExample
+//
+//  Created by Jon Bauer on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct AppClipExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppClipExample/AppClipExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppClipExample
+//
+//  Created by Jon Bauer on 8/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
