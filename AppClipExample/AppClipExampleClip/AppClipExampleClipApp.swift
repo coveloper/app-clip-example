0c765ca135203649584a9a61ad07aa394d450fd9
Jon Bauer <jon@covelop.org>
2022-08-13 17:26:13 -0700

    Create App Clip Target

---
+++ AppClipExample/AppClipExampleClip/AppClipExampleClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipExampleClipApp.swift
+//  AppClipExampleClip
+//
+//  Created by Jon Bauer on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct AppClipExampleClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
